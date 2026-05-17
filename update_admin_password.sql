61f0c32480fe4d7887a82fd91fe7d9d67f9e4588
baymarrxxx <mecd2023-9836-48298@bicol-u.edu.ph>
2026-05-17 21:32:33 +0800

    Delete update_admin_password.sql

--- update_admin_password.sql
+++
@@ -1,8 +0,0 @@
--- Update Admin Password
--- This updates the password hash for admin@rigcheck.com to the correct bcrypt hash for "admin123"
-
-UPDATE User SET password_hash = '$2y$10$c/1.MfAo1IZLLXYSOd7PG.yE27.Ubtr86aLDfcGe0CXzUl6zb6JD2' 
-WHERE email = 'admin@rigcheck.com';
-
--- Verify the update
-SELECT user_id, email, first_name, last_name FROM User WHERE email = 'admin@rigcheck.com';
